608d257b0222627d845e4f1cf93fe8df49b2fddb
tjyuanpeng <tjyuanpeng@gmail.com>
2026-01-13 16:14:19 +0800

    4

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,4 @@
 import { cac } from 'cac'
-import ora from 'ora'
 import replaceFep from './replace-fep.ts'
 
 const cli = cac('fep-migrator')
@@ -7,9 +6,9 @@ cli
   .version('1.0.0')
   .command('<...dir>', '处理路径')
   .action(async (dirs) => {
-    const spinner = ora().start()
+    console.log('# 开始处理.')
     const statistic = await replaceFep(dirs)
-    spinner.info(`所有文件迁移完成。共处理 ${statistic.total} 个文件，成功 ${statistic.success} 个，失败 ${statistic.fail} 个`)
+    console.log(`# 所有文件迁移完成。共处理 ${statistic.total} 个文件，成功 ${statistic.success} 个，失败 ${statistic.fail} 个`)
   })
 cli.help()
 cli.parse()
