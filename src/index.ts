bf7251884b774c17c1d144ef57b95ce14560c42f
tjyuanpeng <tjyuanpeng@gmail.com>
2026-01-13 21:46:22 +0800

    chore: fix publish probles

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
+import process from 'node:process'
 import { cac } from 'cac'
 import replaceFep from './replace-fep'
 
 const cli = cac('fep-migrator')
 cli
-  .version('1.0.0')
+  .version(process.env.npm_package_version ?? '-')
   .command('<...dir>', '处理路径')
   .action(async (dirs) => {
     console.log('# 开始处理.')
